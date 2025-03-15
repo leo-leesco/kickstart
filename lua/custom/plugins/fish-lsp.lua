b7ad797bbfd544eaa7db82b0ee9a3a6b29ae8dfa
Léo LEESCO <leo.leesco@gmail.com>
2025-03-15 19:34:11 +0100

    fix fish-lsp

--- lua/custom/plugins/fish-lsp.lua
+++ lua/custom/plugins/fish-lsp.lua
@@ -1,46 +1,31 @@
 return {
 	'ndonfris/fish-lsp',
 	config = function()
-		vim.api.nvim_create_autocmd('FileType', {
-			pattern = 'fish',
-			callback = function()
-				vim.lsp.start({
-					name = 'fish-lsp',
-					cmd = { 'fish-lsp', 'start' },
-					cmd_env = { fish_lsp_show_client_popups = false },
-				})
-			end,
-		})
+		-- vim.api.nvim_create_autocmd('FileType', {
+		-- 	pattern = 'fish',
+		-- 	callback = function()
+		-- 		vim.lsp.start({
+		-- 			name = 'fish-lsp',
+		-- 			cmd = { 'fish-lsp', 'start' },
+		-- 			cmd_env = { fish_lsp_show_client_popups = false },
+		-- 		})
+		-- 	end,
+		-- })
 
-		local lspconfig = require('lspconfig')
+		-- local lspconfig =
 
 		-- Define the autocommands using the nvim API
-		local group = vim.api.nvim_create_augroup('FishLSP', { clear = true })
+		-- local group = vim.api.nvim_create_augroup('FishLSP', { clear = true })
 
 		vim.api.nvim_create_autocmd({ 'BufWritePre', 'BufEnter' }, {
-			group = group,
+			group = vim.api.nvim_create_augroup('FishLSP', { clear = true }),
 			pattern = '*.fish',
 			callback = function()
-				lspconfig.fish_lsp.setup({
+				require('lspconfig').fish_lsp.setup({
 					cmd = { 'fish-lsp', 'start' },
+					cmd_env = { fish_lsp_show_client_popups = false },
 					filetypes = { 'fish' },
 				})
-			end,
-		})
-
-		vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-			group = group,
-			buffer = vim.api.nvim_get_current_buf(),
-			callback = function()
-				vim.lsp.buf.document_highlight()
-			end,
-		})
-
-		vim.api.nvim_create_autocmd('CursorMoved', {
-			group = group,
-			buffer = vim.api.nvim_get_current_buf(),
-			callback = function()
-				vim.lsp.buf.clear_references()
 			end,
 		})
 	end,
